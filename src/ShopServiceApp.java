bda5cbef704c9cf2f3aa5bffbb1ffb21a30b8f73
Arkadiusz Cichosz <cichorek87220@gmail.com>
2024-07-17 19:33:17 +0200

    Initial commit

---
+++ src/ShopServiceApp.java
@@ -0,0 +1,5 @@
+public class ShopServiceApp {
+    public static void main(String[] args) {
+        System.out.println("ShopServiceApp. Hello world!");
+    }
+}
---
+++ src/domain/Product.java
@@ -0,0 +1,8 @@
+package domain;
+
+public abstract class Product {
+    private String name;
+    private int price;
+    private String description;
+    public abstract void describe();
+}
